1526d5859ad463520993ba373543e048e94ec7bf
Designovationn <133952721+Designovationn@users.noreply.github.com>
2025-04-30 16:08:31 +0530

    style

--- CSUEB-html/js/main.js
+++ CSUEB-html/js/main.js
@@ -29,13 +29,69 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+     // Hero Slider
+    const slideCount = document.querySelectorAll('.hero-slider .swiper-slide').length;
+    const hasMultipleSlides = slideCount > 1;
+    // Show/hide arrows based on the number of slides
+    if (!hasMultipleSlides) {
+        document.querySelector('.swiper-button-group').style.display = 'none';
+    }
+    const swiperhero = new Swiper(".hero-slider", {
+        navigation: hasMultipleSlides ? {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+        } : false,
+    });
 
-    var swiperSlider = new Swiper(".main-single-slider", {
+
+
+    // Signle  Slider
+    var swiperSlider = new Swiper(".main-img-slider", {
         navigation: {
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
         },
     });
+
+    // Header
+    const menuBtn = document.querySelector('.menu-btn');
+    const menuBtnMobile = document.getElementById('menuToggleMobile');
+    const sidebar = document.querySelector('.sidebar');
+    const overlay = document.querySelector('.overlay');
+    const closeBtn = document.querySelector('.close-btn'); // optional
+    const body = document.body;
+    const header = document.querySelector('.main-header');
+
+    // Open Sidebar
+    function openSidebar() {
+      sidebar.classList.add('active');
+      overlay.classList.add('show');
+      body.classList.add('sidebar-open', 'no-scroll');
+    }
+
+    // Close Sidebar
+    function closeSidebar() {
+      sidebar.classList.remove('active');
+      overlay.classList.remove('show');
+      body.classList.remove('sidebar-open', 'no-scroll');
+    }
+
+    // Add event listeners
+    if (menuBtn) menuBtn.addEventListener('click', openSidebar);
+    if (menuBtnMobile) menuBtnMobile.addEventListener('click', openSidebar);
+    if (overlay) overlay.addEventListener('click', closeSidebar);
+    if (closeBtn) closeBtn.addEventListener('click', closeSidebar);
+
+    // Scroll Event - Add class on header when scrolled
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 10) {
+        header.classList.add('header-color');
+      } else {
+        header.classList.remove('header-color');
+      }
+    });
+
+    // Header End
    
 });
   
